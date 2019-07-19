3e40e6bcdec77c4551c7d16d3c0a8e69be46e469
tomk9 <tomaszsz95@gmail.com>
2019-07-19 15:58:52 +0200

    Init

---
+++ android/app/src/main/kotlin/com/szczesny/tomasz/delivery_points/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.szczesny.tomasz.delivery_points
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
